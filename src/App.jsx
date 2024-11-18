9b77c4d7e6e858057668e05f4b1257d83b0ec2e7
gab-braga <gabrielbraga0712@gmail.com>
2024-11-18 16:27:29 -0300

    aula 19

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header/Header"
+
+function Hello() {
+  return <h2>Olá meu amigo. Como vai?</h2>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Olá React!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello World</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+      <Header />
     </>
   )
 }
 
 export default App
+
+// Transpiler (Transpilador) -> Babel
