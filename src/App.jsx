68d58af4b9b3713aaea774ce50ebff9e09bb536b
gab-braga <gabrielbraga0712@gmail.com>
2024-11-21 16:25:16 -0300

    aula 21 parte 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Users from "./pages/Users/Users";
+import Sac from "./pages/Sac/Sac";
 
 function Hello() {
   return <h2>Olá meu amigo. Como vai?</h2>
@@ -10,11 +12,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/sac" element={<Sac />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
 
 // Transpiler (Transpilador) -> Babel
